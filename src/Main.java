0a0eeb0a7e5da36275e5c1349a78d1492d501f1d
shabrat2015 <shabrat2015@gmail.com>
2020-10-28 09:28:35 +0200

    Complite2

--- src/Main.java
+++ src/Main.java
@@ -10,28 +10,32 @@ public class Main {
         shop.add(new Product("AplWathc", 10, 6));
         shop.add(new Product("Shuga", 5, 500));
         shop.add(new Product("GTX 3090 ti FE OC", 1, 50000));
-
+ int prod=1;
         for (Product p : shop)
         {
             p.getProduct();
         }
         ArrayList<Product> basket = new ArrayList<>();
-for(int y=0;y<3;y++) {
+
+    while(prod==1){
     System.out.println("Введите название товара");
     String title;
     Scanner scanner = new Scanner(System.in);
     title = scanner.nextLine();
-   // System.out.println("Введите количество товара");
-  //  int byShet = scanner.nextInt();
+    // System.out.println("Введите количество товара");
+    //  int byShet = scanner.nextInt();
 
     Product p = new Product(title, 0, 0);
     if (shop.contains(p)) {
         int i = shop.indexOf(p);
         basket.add(shop.get(i));
-      //  shop.get(i).deGual(byShet);
+        //  shop.get(i).deGual(byShet);
         shop.get(i).deGual();
     }
+        System.out.println("Продолжить ? (True=1/False=2)");
+    prod=scanner.nextInt();
 }
+
     int nimb=0;
         for (Product tmp: basket)
         {
